cbbd8d435593bd29b62e5133523e5dc2da14c40b
Christopher Scott <chris.c.scott@outlook.com>
2019-10-04 11:37:43 -0500

    added person

--- git-test/src/Main.java
+++ git-test/src/Main.java
@@ -3,7 +3,8 @@ public class Main {
 	
 	public static void main(String args[])
 	{
-		String hello = new String("Hello World");
-		System.out.printf("%s\n", hello);
+		String hello = new String("Hello");
+		String person = new String("Caleb");
+		System.out.printfln(hello + " " + person);
 	}
 }
